859b120c7c378c40fb76e1285b9a3103309e2a00
Pranav Vashi <neobuddy89@gmail.com>
2016-09-19 23:22:27 +0530

    Add Impulse (2016-Edition) Governor
    Signed-off-by: Sai Vishal <saivishal2001@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -417,6 +417,9 @@ extern struct cpufreq_governor cpufreq_gov_lionheart;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HYPER)
 extern struct cpufreq_governor cpufreq_gov_hyper;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_hyper)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_IMPULSE)
+extern struct cpufreq_governor cpufreq_gov_impulse;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_impulse)
 #endif
 
 
