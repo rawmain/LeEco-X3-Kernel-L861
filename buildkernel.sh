7206697221c7838f6d17be7b9e63b9b153f12333
WisniaPL <19wisnia89@gmail.com>
2017-02-20 00:17:47 +0100

    Build script

---
+++ buildkernel.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export ARCH=arm64
+export CONFIG_NO_ERROR_ON_MISMATCH=y
+export CROSS_COMPILE=aarch64-linux-android-
+export PATH=/home/wks/lineage/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH
+#export CONFIG_DEBUG_SECTION_MISMATCH=y
+
+find ./ -name '*~' -o -name '*.bak' | xargs rm;
+rm -rf ~/.ccache;
+make distclean -w V=0 && make mrproper -w V=0;
+echo "Compiling Kernel, using config file $1, this might take long, window will be closed after it is done...";
+echo "You will find errors and warnings of kernel compilation in build_errors_warnings.log file.";
+rm ./.config;
+nice make -w V=0 MTK_TARGET_PROJECT=x500 TARGET_BUILD_VARIANT=user $1 2> ./build_errors_warnings.log;
+make -w -j`grep 'processor' /proc/cpuinfo | wc -l` V=0 2>> ./build_errors_warnings.log;
